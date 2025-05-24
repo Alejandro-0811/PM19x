215038ea99aa7fd4b2470f22acc8b8ae589d4e5e
Alejandro-0811 <122043866@upq.edu.mx>
2025-05-24 16:51:42 -0600

    verificar

---
+++ Practica3/verificar.js
@@ -0,0 +1,19 @@
+function verificarUsuario(usuario) 
+{
+    return new Promise((resolve, reject) => {
+        if (usuario === "admin") {
+            resolve("Acceso concedido");
+        } else {
+            reject("Acceso denegado");
+        }
+    });
+}
+
+// Ejemplo de uso
+verificarUsuario("Alejandro")
+    .then(mensaje => {
+        console.log(mensaje); 
+    })
+    .catch(error => {
+        console.log(error);
+    });
