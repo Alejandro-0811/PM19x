368034e3d4e3dfda316afa7499a63d15b8112ba0
Alejandro-0811 <122043866@upq.edu.mx>
2025-06-10 15:37:46 -0600

    avance practica 4

---
+++ my-app2/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+const Texto= ()=> {
+  return (
+    <Text>Hola Mundo React Native</Text>)
+
+    }
+
+export default function App() {
+  return (
+
+
+    <View style={styles.container}>
+
+      <Texto></Texto>
+      <StatusBar style="auto" />
+      <Button title="Presioname"></Button>
+      <Texto></Texto>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
